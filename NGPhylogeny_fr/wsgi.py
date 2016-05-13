22f79130d0b859e3917e29b21622ac2822e06100
DamCorreia <correia.damien@gmail.com>
2016-05-13 15:34:57 +0200

    first commit

---
+++ NGPhylogeny_fr/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for NGPhylogeny_fr project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NGPhylogeny_fr.settings")
+
+application = get_wsgi_application()
