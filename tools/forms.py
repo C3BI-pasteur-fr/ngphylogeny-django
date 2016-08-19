f0a27fc4094ce007a3a6406afd6eb6d620a05f0a
DamCorreia <correia.damien@gmail.com>
2016-08-19 16:32:41 +0200

    tool form update

--- tools/forms.py
+++ tools/forms.py
@@ -4,7 +4,7 @@ from crispy_forms.layout import Submit, Layout, Field, Div
 from django import forms
 
 
-def mapGalaxyToolInput(attr):
+def map_galaxy_tool_input(attr):
         """take Galaxy Tool build information return django field dict"""
 
         field_map = dict()
@@ -24,42 +24,49 @@ def mapGalaxyToolInput(attr):
 
 class ToolForm(forms.Form):
 
+    n = 0
+    fieds_ids_mapping = {}
+
     def create_field(self, attrfield):
 
+        self.n +=1
+        field_id = str(self.n)
         fieldtype = attrfield.get("type", "")
-        fieldname = attrfield.get("name", "")
 
         if fieldtype == "data":
-            self.fields[fieldname] = forms.FileField(**mapGalaxyToolInput(attrfield))
+            self.fields[field_id] = forms.FileField(**map_galaxy_tool_input(attrfield))
 
         elif fieldtype == "select":
-            self.fields[fieldname] = forms.ChoiceField(**mapGalaxyToolInput(attrfield))
+            if attrfield.get("display", "") == 'radio':
+                self.fields[field_id] = forms.ChoiceField(widget=forms.RadioSelect, **map_galaxy_tool_input(attrfield))
+            else:
+                self.fields[field_id] = forms.ChoiceField(**map_galaxy_tool_input(attrfield))
 
         elif fieldtype == "boolean":
-            data_onvalue = attrfield.pop("truevalue", None)
-            data_offvalue = attrfield.pop("falsevalue", None)
+            data_onvalue = attrfield.get("truevalue", None)
+            data_offvalue = attrfield.get("falsevalue", None)
 
-            self.fields[fieldname] = forms.CharField(widget=forms.CheckboxInput(
-                                                     attrs={'data-toggle': "toggle",
-                                                            'data-on': 'Yes',
-                                                            'data-off': 'No',
-                                                            'data-on-value': data_onvalue,
-                                                            'data-off-value': data_offvalue,
-                                                            }),
-                                                     **mapGalaxyToolInput(attrfield))
+            self.fields[field_id] = forms.CharField(widget=forms.CheckboxInput(
+                                                    attrs={'data-toggle': "toggle",
+                                                           'data-on': 'Yes',
+                                                           'data-off': 'No',
+                                                           'data-on-value': data_onvalue,
+                                                           'data-off-value': data_offvalue,
+                                                           }),
+                                                    **map_galaxy_tool_input(attrfield))
 
         elif fieldtype == "text":
-            self.fields[fieldname] = forms.CharField(**mapGalaxyToolInput(attrfield))
+            self.fields[field_id] = forms.CharField(**map_galaxy_tool_input(attrfield))
 
         elif fieldtype == "integer":
-            self.fields[fieldname] = forms.IntegerField(**mapGalaxyToolInput(attrfield))
+            self.fields[field_id] = forms.IntegerField(**map_galaxy_tool_input(attrfield))
 
         else:
-            self.fields[fieldname] = forms.CharField(**mapGalaxyToolInput(attrfield))
+            self.fields[field_id] = forms.CharField(**map_galaxy_tool_input(attrfield))
 
-        return fieldname
+        return field_id
 
-    def parse_galaxy_input_tool(self, list_inputs):
+    def parse_galaxy_input_tool(self, list_inputs, cond_name=''):
 
         fields_created = []
 
@@ -67,22 +74,27 @@ class ToolForm(forms.Form):
             cond_input = input_tool.get('test_param')
 
             if cond_input:
-                nested_field = []
-                new_name_field = self.create_field(cond_input)
+                cond_name = input_tool.get('name') + '|'
 
+                conditional_field = self.create_field(cond_input)
+                self.fieds_ids_mapping[conditional_field] = cond_name+cond_input['name']
+
+                nested_field = []
                 cases = input_tool.get('cases', '')
                 for case in cases:
-
                     case_inputs = case.get('inputs')
                     if case_inputs:
                         case_value = case.get('value')
-                        nested_field.append(Div(id='id_'+new_name_field+"_cond_"+case_value,
-                                                *self.parse_galaxy_input_tool(case_inputs)))
+                        nested_field.append(Div(data_test=conditional_field, data_case=case_value, css_class="well",
+                                                *self.parse_galaxy_input_tool(case_inputs, cond_name)))
 
-                fields_created.append(Div(new_name_field, *nested_field))
+                fields_created.append(Div(conditional_field, *nested_field))
+                cond_name = ''
 
             else:
-                fields_created.append(Field(self.create_field(input_tool)))
+                new_field = self.create_field(input_tool)
+                fields_created.append(Field(new_field))
+                self.fieds_ids_mapping[new_field] = cond_name + input_tool['name']
 
         return fields_created
 
@@ -91,11 +103,9 @@ class ToolForm(forms.Form):
 
         self.helper = FormHelper(self)
         self.helper.form_class = 'blueForms'
-
         self.formset = self.parse_galaxy_input_tool(tool_params)
-        FIELDS = self.formset
-
-        self.helper.layout = Layout(FormActions(Field(*FIELDS),
-                                                Submit('submit', 'Submit'),
-                                            )
+        self.helper.layout = Layout(FormActions(Field(*self.formset),
+                                                Submit('submit', 'Submit', css_class="pull-right"),
+                                                )
                                     )
+
--- tools/views.py
+++ tools/views.py
@@ -1,5 +1,9 @@
+import tempfile
+
+from bioblend.galaxy.tools.inputs import inputs
+from django.core.urlresolvers import reverse_lazy
 from django.http import JsonResponse
-from django.shortcuts import render ,get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import DetailView, ListView
 
 from account.decorator import connection_galaxy
@@ -10,31 +14,93 @@ from .models import Tool
 class ToolListView(ListView):
     model = Tool
 
+
 class ToolDetailView(DetailView):
     model = Tool
 
 
 class ToolJSONView(ToolDetailView):
-
     def get(self, request, *args, **kwargs):
-
         tool = self.get_object()
         jsondata = {'id': tool.id, 'name': tool.name,
-        'id_galaxy': tool.id_galaxy,
-        'inputs_data': list(str(i) for i in tool.input_data),
-        'outputs_data': list(str(o) for o in tool.output_data),
-        'tool_linkable': list(str(t) for t in tool.compatible_tool),
-        }
+                    'id_galaxy': tool.id_galaxy,
+                    'inputs_data': list(str(i) for i in tool.input_data),
+                    'outputs_data': list(str(o) for o in tool.output_data),
+                    'tool_linkable': list(str(t) for t in tool.compatible_tool),
+                    }
         return JsonResponse(jsondata)
 
 
 @connection_galaxy
 def tool_form_view(request, pk):
-
     tool_obj = get_object_or_404(Tool, pk=pk)
     data = request.galaxy.tools.show_tool(tool_id=tool_obj.id_galaxy, io_details='true')
 
     context = {"toolform": ToolForm(data['inputs']),
                "tool": tool_obj}
+
+    return render(request, 'tools/tool_form.html', context)
+
+
+@connection_galaxy
+def tool_exec_view(request, pk):
+    gi = request.galaxy
+    message = ""
+
+    tool_obj = get_object_or_404(Tool, pk=pk)
+    tool_inputs_details = gi.tools.show_tool(tool_id=tool_obj.id_galaxy, io_details='true')
+    tool_form = ToolForm(tool_inputs_details['inputs'], request.POST or None)
+
+    if request.method == 'POST':
+
+        if tool_form.is_valid():
+
+            try:
+                history_id = gi.histories.get_most_recently_used_history().get('id')
+
+            except:
+                # Create a new galaxy history and delete older if the user is not authenticated
+                history_id = gi.histories.create_history().get('id')
+
+            tool_inputs = inputs()
+            for key, value in request.POST.items():
+                tool_inputs.set_param(tool_form.fieds_ids_mapping.get(key), value)
+
+            if request.FILES:
+                for input_file_id in request.FILES:
+
+                    uploaded_file = request.FILES.get(input_file_id)
+                    tmp_file = tempfile.NamedTemporaryFile()
+                    for chunk in uploaded_file.chunks():
+                        tmp_file.write(chunk)
+                    tmp_file.flush()
+
+                    # send file to galaxy
+                    outputs = gi.tools.upload_file(tmp_file.name, history_id, file_name=uploaded_file.name)
+                    file_id = outputs.get('outputs')[0].get('id')
+                    tool_inputs.set_dataset_param(tool_form.fieds_ids_mapping.get(input_file_id.strip('[]')), file_id)
+
+            try:
+                tool_outputs = gi.tools.run_tool(history_id=history_id,
+                                                 tool_id=tool_obj.id_galaxy,
+                                                 tool_inputs=tool_inputs)
+
+                return redirect(reverse_lazy("history_detail", kwargs={'history_id': history_id}))
+
+            except Exception as e:
+                import ast
+                from django.forms import ValidationError
+
+                message = ast.literal_eval(e.body)
+
+                reverse_dict_field = {v: k for k, v in tool_form.fieds_ids_mapping.items()}
+
+                for field, err_msg in message.get('err_data').items():
+                    tool_form.add_error(reverse_dict_field.get(field),
+                                        ValidationError(err_msg, code='invalid'))
+
+    context = {"toolform": tool_form,
+               "tool": tool_obj,
+               }
 
     return render(request, 'tools/tool_form.html', context)
