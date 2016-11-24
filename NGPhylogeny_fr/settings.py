e92cea0ba985a7319578bb2e758557fad03eceff
damcorreia <correia.damien@gmail.com>
2016-11-24 13:36:29 +0100

    import galaxyconf

--- NGPhylogeny_fr/settings.py
+++ NGPhylogeny_fr/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-import galaxyconf
+from galaxyconf import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
