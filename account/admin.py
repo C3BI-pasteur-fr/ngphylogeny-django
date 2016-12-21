18f4de1b14ef460b2954edaa153e14303735ca38
damcorreia <correia.damien@gmail.com>
2016-12-21 16:48:21 +0100

    Galaxy conf admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+admin.site.register(GalaxyServer)
+admin.site.register(GalaxyConf)
+admin.site.register(GalaxyUser)
