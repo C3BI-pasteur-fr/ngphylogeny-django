f3059a63fcff4015d33483a2beca943dfba14a27
damcorreia <correia.damien@gmail.com>
2017-01-11 19:18:31 +0100

    add features admin account

--- account/admin.py
+++ account/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
+class GalaxyConfAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'galaxy_server', 'active')
+
 
 admin.site.register(GalaxyServer)
-admin.site.register(GalaxyConf)
+admin.site.register(GalaxyConf, GalaxyConfAdmin)
 admin.site.register(GalaxyUser)
