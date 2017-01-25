82dbfe70858b47d1f26fdf2ce8702fa8c48dbd48
damcorreia <correia.damien@gmail.com>
2017-01-25 12:35:34 +0100

    remove commented code, save galaxyconf call in the request object

--- account/decorator.py
+++ account/decorator.py
@@ -3,26 +3,6 @@ from __future__ import unicode_literals
 from django.shortcuts import redirect, get_object_or_404
 from account.models import GalaxyConf, GalaxyUser
 
-# from views import galaxy_connection_error_view
-# import requests
-# from galaxylib import GalaxyInstanceAnonymous
-# from django.conf import settings
-#
-# Allow anonymous access deprecated
-# def get_galaxy_session_id():
-#     """
-#     Open an anonymous connection with Galaxy server and save Galaxy session information
-#     """
-#
-#     connection = requests.get(settings.GALAXY_SERVER_URL)
-#     # connect to galaxy server and retrieve galaxysession ID stored in a cookie
-#     if connection.status_code == 200:
-#         return connection.cookies.get('galaxysession')
-#
-#     else:
-#         # TODO Galaxy no response page
-#         raise Exception("Galaxy server response:" + connection.status.code)
-
 
 def connection_galaxy(view_function):
     """Initiating Galaxy connection"""
@@ -30,17 +10,26 @@ def connection_galaxy(view_function):
     def wrapper(request, *args, **kwargs):
 
         try:
-            galaxy_conf = get_object_or_404(GalaxyConf, active=True)
-        except:
 
+            if hasattr(request, 'galaxyconf'):
+                galaxy_conf = request.galaxyconf
+
+            else:
+                galaxy_conf = get_object_or_404(GalaxyConf, active=True)
+                request.galaxyconf = galaxy_conf
+
+        except Exception, e:
+            print e
             raise Exception("NGPhylogeny server is not properly configured,"
-                   "please ensure that the Galaxy server is correctly set up")
+                            "please ensure that the Galaxy server is correctly set up")
 
         if request.user.is_authenticated():
             """Try to use related Galaxy user information"""
 
             try:
-                gu = GalaxyUser.objects.get(user=request.user, galaxy_server__galaxyconf__active=True )
+                """get or create galaxy user onfly"""
+                gu, created = GalaxyUser.objects.get_or_create(user=request.user, galaxy_server=galaxy_conf.galaxy_server)
+
                 """If the key api is not defined, prompts the user to define it"""
                 if gu.api_key:
                     request.galaxy = gu.get_galaxy_instance()
@@ -48,30 +37,16 @@ def connection_galaxy(view_function):
                     return redirect('account')
 
             except Exception, e:
-                """create galaxy user onfly"""
-                gu = GalaxyUser(user=request.user, galaxy_server=galaxy_conf.galaxy_server)
-                gu.save()
+                print e
                 return redirect('account')
 
         elif request.user.is_anonymous():
-
             """If user is not an authenticated, use anonymous galaxyuser set"""
             try:
-
                 request.galaxy = galaxy_conf.anonymous_user.get_galaxy_instance()
 
             except:
                 return redirect('account')
-
-                # Allow anonymous access deprecated
-                # """If user is not an authenticated galaxyuser use cookies"""
-                # try:
-                #    id_galaxysession = request.session.setdefault(settings.GALAXY_SESSION_ID, get_galaxy_session_id())
-                #    request.galaxy = GalaxyInstanceAnonymous(url=settings.GALAXY_SERVER_URL, galaxysession=id_galaxysession)
-                #
-                # except requests.ConnectionError:
-                #
-                #    return galaxy_connection_error_view(request)
 
         return view_function(request, *args, **kwargs)
 
