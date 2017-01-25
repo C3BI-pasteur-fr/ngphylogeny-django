bd17928cc913561c18106c32fc8aa869110c50bc
damcorreia <correia.damien@gmail.com>
2017-01-25 14:21:03 +0100

    increase varchar limit : tool version

--- tools/models.py
+++ tools/models.py
@@ -22,7 +22,7 @@ class Tool(models.Model):
     id_galaxy = models.CharField(max_length=250, unique=True)
     toolshed = models.CharField(max_length=100, blank=True)
     name = models.CharField(max_length=100, blank=True)
-    version = models.CharField(max_length=10, blank=True)
+    version = models.CharField(max_length=20, blank=True)
     description = models.CharField(max_length=250)
 
     @classmethod
