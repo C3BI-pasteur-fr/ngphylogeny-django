b622469ab69b232ad9429611f240bc99a52856e4
damcorreia <correia.damien@gmail.com>
2017-10-12 15:32:13 +0200

    add preview of history inside admin interface

--- workspace/admin.py
+++ workspace/admin.py
@@ -4,7 +4,20 @@ from .models import WorkspaceHistory
 
 
 class WorkspaceAdmin(admin.ModelAdmin):
+    change_form_template = "workspace/admin/custom_change_form.html"
+    list_display = ('name', 'history', 'user', 'created_date', 'galaxy_server',)
 
-    list_display = ('name','history','user','created_date', 'galaxy_server',)
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        obj = self.get_object(request, object_id)
+        extra_context = extra_context or {}
+        gu = obj.get_galaxy_user()
+        if gu:
+            gi = gu.get_galaxy_instance
+            gi.nocache = True
+            extra_context['history_content'] = gi.histories.show_history(obj.history, contents=True)
 
-admin.site.register(WorkspaceHistory,WorkspaceAdmin)
+        return super(WorkspaceAdmin, self).change_view(request, object_id, form_url,
+                                                       extra_context=extra_context, )
+
+
+admin.site.register(WorkspaceHistory, WorkspaceAdmin)
