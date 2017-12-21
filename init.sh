ef88e3a9aa18c27c10cf8f2bf51966b9b205f23a
damcorreia <correia.damien@gmail.com>
2017-12-21 15:10:07 +0100

    update requirement apps version

--- init.sh
+++ init.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+GALAXYURL=${1-'https://galaxy.pasteur.fr'}
+
+pip install virtualenv
+virtualenv ENV
+source ENV/bin/activate
 pip install -r requirement.txt
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata tool_flags
-python manage.py importtools
+python manage.py importtools --galaxyurl $GALAXYURL --query "phylogeny"
 python manage.py compute_tools_links
