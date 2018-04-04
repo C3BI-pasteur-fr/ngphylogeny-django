14189eff87a8ee1c015b3c036d8d107a820b7978
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2018-04-04 12:05:35 +0200

    Removed debug_toolbar

--- NGPhylogeny_fr/settings/local.py
+++ NGPhylogeny_fr/settings/local.py
@@ -4,13 +4,13 @@ DEBUG = True
 
 ROOT_URLCONF = 'NGPhylogeny_fr.urls.local'
 
-INSTALLED_APPS += (
-    'debug_toolbar',
-)
+# INSTALLED_APPS += (
+#     'debug_toolbar',
+# )
 
-MIDDLEWARE += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
+# MIDDLEWARE += (
+#     'debug_toolbar.middleware.DebugToolbarMiddleware',
+# )
 
 DATABASES = {
     'default': {
