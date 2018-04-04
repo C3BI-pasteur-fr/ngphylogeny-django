1cb48e67939e8029dc99fcd1da4ee9300ed49998
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2018-04-04 17:28:51 +0200

    Added command to import workflows

--- startup.sh
+++ startup.sh
@@ -13,4 +13,5 @@ python manage.py creategalaxyserver --url=$GALAXYSERVER --activate
 echo "from django.contrib.auth.models import User; from galaxy.models import Server, GalaxyUser; u = User.objects.filter(username='admin'); g = Server.objects.filter(url='$GALAXYSERVER'); print(u);print(g);gu=GalaxyUser(user=u.first(),galaxy_server=g.first(),api_key='$GALAXYKEY',anonymous=True);gu.save()" | python manage.py shell
 python manage.py importtools --galaxyurl=$GALAXYSERVER --query="phylogeny" --flags=toolflags.txt --force
 python manage.py import_links --linkfile=toollinks.txt
+python manage.py importworkflows --galaxyurl=http://localhost:8080
 python manage.py runserver 0.0.0.0:8000
---
+++ tools/management/commands/importworkflows.py
@@ -0,0 +1,63 @@
+import requests
+import re
+from django.core.management.base import BaseCommand, CommandError
+from slugify import slugify
+
+from galaxy.models import Server
+from galaxy.models import GalaxyUser
+from workflows.models import Workflow
+
+class Command(BaseCommand):
+    help = 'Import Galaxy workflows into NGPhylogeny'
+    requires_system_checks = True
+    flags = []
+
+    def add_arguments(self, parser):
+        # Named (optional) arguments
+        parser.add_argument('--galaxyurl')
+
+
+    def handle(self, *args, **options):
+        galaxy_url = options.get('galaxyurl')
+        self.import_workflows(galaxy_url)
+
+    def import_workflows(self, galaxy_url):
+        if galaxy_url:
+            galaxy_server, created = Server.objects.get_or_create(
+                url=galaxy_url)
+        else:
+            try:
+                galaxy_server = Server.objects.get(current=True)
+            except Server.DoesNotExist:
+                raise CommandError(
+                    'Server Galaxy does not exist, please use --galaxyurl')
+
+        galaxy_key = GalaxyUser.objects.filter(galaxy_server__url=galaxy_url, anonymous = True)
+        print(galaxy_key)
+        workflows_url = '%s/%s/%s/?key=%s' % (galaxy_server.url, 'api', 'workflows',galaxy_key.first().api_key)
+
+        # fetch list of tools
+        connection = requests.get(workflows_url)
+        print(workflows_url)
+        print(connection.status_code)
+        if connection.status_code == 200:
+            wf_list = connection.json() or []
+            for wf in wf_list:
+                wfname = wf.get('name')
+                wfid = wf.get('id')
+                if re.search('oneclick', wfname, re.IGNORECASE):
+                    w = Workflow(galaxy_server = galaxy_server, id_galaxy = wfid, name = wfname, description = wfname, slug = slugify(wfname) )
+                    w.save()
+        else:
+            self.stdout.write("Problem while querying galaxy server")
+
+    def associate_flags(self, flagfile):
+        for flaglink in self.flags:
+            tool = Tool.objects.filter(
+                name=flaglink[0]).first()
+            for f in flaglink[1].split(","):
+                self.stdout.write(self.style.SUCCESS(
+                    "%s : %s" % (flaglink[0], f)))
+                flag = ToolFlag.objects.filter(
+                    verbose_name=f).first()
+                flag.tool.add(tool)
