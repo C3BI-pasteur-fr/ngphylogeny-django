7e126819d2c70346e5640ee297e5500ae6aeb144
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2018-04-04 11:07:01 +0200

    Corrected init.sh

--- init.sh
+++ init.sh
@@ -7,8 +7,8 @@ GALAXYSERVER=$3
 pip install -r requirement.txt
 python manage.py makemigrations
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.filter(email='$USEREMAIL').delete(); User.objects.create_superuser('admin', '$USEREMAIL', '$USERPASS')" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').delete(); User.objects.create_superuser('admin', '$USEREMAIL', '$USERPASS');exit()" | python manage.py shell
 python manage.py loaddata tool_flags
 python manage.py creategalaxyserver --url=$GALAXYSERVER --activate
-python manage.py importtools --galaxyurl=$GALAXYSERVER --query="phylogeny" --flags=toolflags.txt
+python manage.py importtools --galaxyurl=$GALAXYSERVER --query="phylogeny" --flags=toolflags.txt --force
 python manage.py import_links --linkfile=toollinks.txt
