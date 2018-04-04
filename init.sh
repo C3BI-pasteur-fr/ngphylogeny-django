1c2b097f28920397f3256497224df278323243e2
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2018-04-04 10:48:51 +0200

    Modified init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+USERPASS=$1
+USEREMAIL=$2
+GALAXYSERVER=$3
+
 pip install -r requirement.txt
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser
+echo "from django.contrib.auth.models import User; User.objects.filter(email='$USEREMAIL').delete(); User.objects.create_superuser('admin', '$USEREMAIL', '$USERPASS')" | python manage.py shell
 python manage.py loaddata tool_flags
-python manage.py creategalaxyserver --interactive --activate
-python manage.py importtools --query "phylogeny"
-python manage.py compute_tools_links
-
+python manage.py creategalaxyserver --url=$GALAXYSERVER --activate
+python manage.py importtools --galaxyurl=$GALAXYSERVER --query="phylogeny" --flags=toolflags.txt
+python manage.py import_links --linkfile=toollinks.txt
