7f1bcbbe0b63878e1ea71b3733a16abdca7af360
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2019-01-14 12:00:45 +0100

    Documentation

--- static/js/ngphylo.js
+++ static/js/ngphylo.js
@@ -8,3 +8,18 @@ function init(){
 $(document).ready(function(){
     init();
 });
+
+function toc(){
+    var toc="";
+    $("h2, h3").each(function(i) {
+	var current = $(this);
+	current.attr("id", "title" + i);
+	toc+="<p class='toc-"+current.prop("tagName")+"'><a id='link" + i + "' href='#title" +
+	    i + "' title='" + current.prop("tagName") + "'>" + 
+	    current.html() + "</a></p>";
+	console.log(current.html());
+	current.html('<a class="anchor-title" aria-label="Anchor link for: glyphicons how to use" href="#title'+i+'" class="anchorjs-link ">'+current.html()+'</a>');
+    });
+    $("#toc").append('<h2>Table of contents</h2>')
+    $("#toc").append(toc);
+}
